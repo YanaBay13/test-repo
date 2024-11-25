7aca55388e0e25c6317fb228ce8531af3e15ed33
YanaBay13 <yana.bay.2004@gmail.com>
2024-11-25 19:21:21 +0500

    Update test 1.py
    Поменяла 1 задачу на правильную

--- laba 1/test 1.py
+++ laba 1/test 1.py
@@ -1,19 +1,18 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+# TODO Найдите количество книг, которое можно разместить на дискете
+# Данные задачи
+diskette_size_mb = 1.44  # Объём дискеты в мегабайтах
+pages_per_book = 100     # Количество страниц в книге
+lines_per_page = 50      # Число строк на странице
+chars_per_line = 25      # Количество символов в строке
+bytes_per_char = 4       # Объём одного символа в байтах
 
-# TODO заменить значение пропущенного элемента средним арифметическим
+# Расчёты
+diskette_size_bytes = diskette_size_mb * 1024 * 1024
+total_chars_per_book = pages_per_book * lines_per_page * chars_per_line
+book_size_bytes = total_chars_per_book * bytes_per_char
 
-# Найти индекс пропущенного элемента
-missing_index = numbers.index(None)
+# Количество книг, которые поместятся
+books_fit = int(diskette_size_bytes // book_size_bytes)
 
-# Рассчитать сумму и количество для среднего арифметического (без пропущенного элемента)
-sum_without_missing = sum(num for num in numbers if num is not None)
-count_with_missing = len(numbers)
-
-# Вычислить среднее арифметическое
-average = sum_without_missing / (count_with_missing)
-
-# Заменить пропущенный элемент
-numbers[missing_index] = average
-
-# Вывод обновлённого списка
-print("Измененный список:", numbers)
+# Результат
+print(f"Количество книг, помещающихся на дискету: {books_fit}")
