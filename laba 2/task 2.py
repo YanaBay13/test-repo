dfc7990bb53a6e83541a45c97910199beb45f148
YanaBay13 <yana.bay.2004@gmail.com>
2024-11-25 20:05:40 +0500

    Update task 2.py
    Добрый вечер, исправила

--- laba 2/task 2.py
+++ laba 2/task 2.py
@@ -2,7 +2,13 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
-
-print("Количество месяцев, которое можно протянуть без долгов:", 8)
+month = 0
+while True:
+    delta = spend - salary
+    if delta > money_capital:
+        break
+    month += 1
+    money_capital -= delta
+    spend *= 1 + increase
+print("Количество месяцев, которое можно протянуть без долгов:", month)
